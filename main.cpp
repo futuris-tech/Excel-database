d918db2888f733e724c1554ab26b309db02cb551
futuris-tech <90894182+futuris-tech@users.noreply.github.com>
2022-10-13 22:53:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,21 @@
 #include <chrono>
 #include <OpenXLSX.hpp>
 #include <time.h>
+#include <string.h>
+#include <array>
 #include "sha256.h"
+#include "head.h"
+using namespace std;
 using namespace OpenXLSX;
 
-const int Count = 1000;
+const int Count = 10000;
 
-extern const char* const m_names[];
-extern const char* const f_names[];
-extern const char* const surnames[];
-extern const char* const cities[];
-extern const char* const countries[];
-extern const char* const classes[];
-extern const int m_name_num;
-extern const int f_name_num;
-extern const int surname_num;
-extern const int city_num;
-extern const int class_num;
-
+extern const array<const char*, 40> m_names;
+extern const array<const char*, 44> f_names;
+extern const array<const char*, 250> surnames;
+extern const array<place, 209> cities;
+extern const array<place, 16> summer;
+extern const array<const char*, 3> classes;
 
 class randomaizer {
 	struct city {
@@ -35,25 +33,23 @@ public:
 	uint32_t operator()() { return dre(); }
 	inline uint32_t get_small(uint32_t range, int pow = 1) {
 		uint64_t value = dre();
-		do {
-			value *= value;
-			value /= dre.max();
-		} while (--pow);
-		return (value * range) >> 32;
+		do value = value * value / dre.max();
+		while (--pow);
+		return value * range / dre.max();
 	}
 
 	char* get_name() {
 		static char buffer[128];
 		const char* input;
 		bool m = dre() % 2;
-		if (m)	input = m_names[dre() % m_name_num];
-		else	input = f_names[dre() % f_name_num];
+		if (m)	input = m_names[dre() % m_names.size()];
+		else	input = f_names[dre() % f_names.size()];
 		strcpy(buffer, input);
 
 		auto ptr = buffer + strlen(input);
 		*ptr++ = ' ';
 
-		input = surnames[dre() % surname_num];
+		input = surnames[dre() % surnames.size()];
 		strcpy(ptr, input);
 
 		ptr += strlen(input);
@@ -69,44 +65,67 @@ public:
 int main() {
 	XLDocument doc;
 	doc.create("Spreadsheet.xlsx");
-	auto wks = doc.workbook().worksheet("Sheet1");
+	auto wb = doc.workbook();
+	wb.addWorksheet("Sheet2");
+	auto wks1 = wb.worksheet("Sheet1");
+	auto wks2 = wb.worksheet("Sheet2");
 
-	wks.column(1).setWidth(21);
-	wks.column(2).setWidth(8);
-	wks.column(3).setWidth(11);
-	wks.column(4).setWidth(17);
-	wks.column(5).setWidth(16);
-	wks.column(6).setWidth(16);
-	wks.column(7).setWidth(12);
-	wks.column(8).setWidth(10);
-	wks.column(9).setWidth(11);
-	wks.column(10).setWidth(65);
+	wks1.cell(1, 1).value() = u8"Имя";
+	wks1.cell(1, 2).value() = u8"Возраст";
+	wks1.cell(1, 3).value() = u8"Паспорт";
+	wks1.cell(1, 4).value() = u8"Гражданство";
+	wks1.cell(1, 5).value() = u8"SHA256";
 
-	wks.cell(1, 1).value() = u8"Имя";
-	wks.cell(1, 2).value() = u8"Возраст";
-	wks.cell(1, 3).value() = u8"Паспорт";
-	wks.cell(1, 4).value() = u8"Гражданство";
-	wks.cell(1, 5).value() = u8"Откуда";
-	wks.cell(1, 6).value() = u8"Куда";
-	wks.cell(1, 7).value() = u8"Стоимость, $";
-	wks.cell(1, 8).value() = u8"Класс";
-	wks.cell(1, 9).value() = u8"Дата";
-	wks.cell(1, 10).value() = u8"SHA256";
+	wks1.column(1).setWidth(21);
+	wks1.column(2).setWidth(8);
+	wks1.column(3).setWidth(11);
+	wks1.column(4).setWidth(17);
+	wks1.column(5).setWidth(65);
+
+	wks2.cell(1, 1).value() = u8"Откуда";
+	wks2.cell(1, 2).value() = u8"Куда";
+	wks2.cell(1, 3).value() = u8"Стоимость, $";
+	wks2.cell(1, 4).value() = u8"Класс";
+	wks2.cell(1, 5).value() = u8"Дата";
+	wks2.cell(1, 6).value() = u8"SHA256";
+
+	wks2.column(1).setWidth(16);
+	wks2.column(2).setWidth(16);
+	wks2.column(3).setWidth(12);
+	wks2.column(4).setWidth(10);
+	wks2.column(5).setWidth(11);
+	wks2.column(6).setWidth(65);
 
 	std::cout.precision(3);
 	std::cout.width(8);
 	randomaizer dre;
 	for (int i = 2; i <= Count+1; i++) {
-		int city_id[] = { dre() % city_num, dre() % city_num };
+		char date[128];
+		time_t raw = time(0) + dre.get_small(365 * 60*60*24);
+		tm info = *localtime(&raw);
+		strftime(date, 128, "%d.%m.%Y", &info);
+
+		place places[2];
 		const char* country;
-		auto r = dre() % 11;
-		if (r < 5)		country = countries[city_id[0]];
-		else if (r < 10)country = countries[city_id[1]];
-		else			country = countries[dre() % city_num];
+		if (info.tm_mon >= 5 && info.tm_mon <= 9 && dre() % 3) {
+			places[0] = cities[dre() % (int)cities.size()];
+			places[1] = summer[dre() % (int)summer.size()];
+			country = places[0].country;
+			if (dre() % 2)
+				std::swap(places[0], places[1]);
+		}
+		else {
+			places[0] = cities[dre() % (int)cities.size()];
+			places[1] = cities[dre() % (int)cities.size()];
+			auto r = dre() % 11;
+			if (r < 5)		country = places[0].country;
+			else if (r < 10)country = places[1].country;
+			else			country = cities[dre() % summer.size()].country;
+		}
 
 		auto name = dre.get_name();
 		auto age = dre() % 85 + 5;
-		auto class_id = dre.get_small(class_num, 2);
+		auto class_id = dre.get_small(classes.size(), 2);
 		int cost;
 		switch (class_id) {
 		case 0:
@@ -122,23 +141,21 @@ int main() {
 			cost = 0;
 		}
 
-		char date[128];
-		time_t raw = time(0) + dre.get_small(365 * 60*60*24, 3);
-		tm info = *localtime(&raw);
-		strftime(date, 128, "%d.%m.%Y", &info);
 
-		wks.cell(i, 1).value() = name;
-		wks.cell(i, 2).value() = age;
-		wks.cell(i, 3).value() = (dre() ^ (dre() << 16)) % 99999999 + 500000000;
-		wks.cell(i, 4).value() = country;
-		wks.cell(i, 5).value() = cities[city_id[0]];
-		wks.cell(i, 6).value() = cities[city_id[1]];
-		wks.cell(i, 7).value() = cost;
-		wks.cell(i, 8).value() = classes[class_id];
-		wks.cell(i, 9).value() = date;
+		wks1.cell(i, 1).value() = name;
+		wks1.cell(i, 2).value() = age;
+		wks1.cell(i, 3).value() = (dre() ^ (dre() << 16)) % 99999999 + 500000000;
+		wks1.cell(i, 4).value() = country;
+		wks2.cell(i, 1).value() = places[0].city;
+		wks2.cell(i, 2).value() = places[1].city;
+		wks2.cell(i, 3).value() = cost;
+		wks2.cell(i, 4).value() = classes[class_id];
+		wks2.cell(i, 5).value() = date;
 
-		itoa(age, name + strlen(name), 10);
-		wks.cell(i, 10).value() = sha256(name);
+		*(int*)(name + strlen(name)) = age;
+		auto sha = sha256(name);
+		wks1.cell(i, 5).value() = sha;
+		wks2.cell(i, 6).value() = sha;
 
 		float progress = (i - 1) * (100.0f / Count);
 		printf("\r%.1f%%", progress);
